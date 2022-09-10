1a233d4d0132fe6f6dec72d3b6380453b0f1addc
leonardo_mendieta <leonardo_mendieta@yahoo.com>
2022-09-10 07:42:28 -0500

    Proyecto: my-app, Descripcion: Version final del proyecto

---
+++ src/main/java/com/leo/myapp/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.leo.myapp.entity;
+
+/**
+ * @author leonardo mendieta
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad que representa el nombre del test
+	 */
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
